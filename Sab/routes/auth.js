89c56315bafd4a75b44e7ad095a61a082a528502
Errorpat-404 <nadgesachin644@gmail.com>
2022-11-10 14:01:10 +0530

    Last Commit

--- Sab/routes/auth.js
+++
@@ -1,5 +0,0 @@
-const express=require("express")
-const router=express.Router()
-const AuthControlller=require("../controllers/AuthControllers");
-router.post("/register",AuthControlller.register)
-module.exports=router
